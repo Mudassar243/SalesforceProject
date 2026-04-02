c6286d3abeb9669e409dc0fbf30a74fe48d2cf16
Muhammad Mudassar <mudassarzafar243243@gmail.com>
2026-04-02 18:06:55 +0500

    Completed

--- force-app/main/default/lwc/contactList/contactList.js
+++ force-app/main/default/lwc/contactList/contactList.js
@@ -1,3 +1,39 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track, wire } from 'lwc';
+import getPendingFollowUps from '@salesforce/apex/FollowUpController.getPendingFollowUps';
+import markCompleted from '@salesforce/apex/FollowUpController.markCompleted';
+import { refreshApex } from '@salesforce/apex';
 
-export default class ContactList extends LightningElement {}
+export default class ContactList extends LightningElement {
+
+    @track contacts = [];
+    notesMap = {};
+    wiredResult; 
+
+    @wire(getPendingFollowUps)
+    wiredContacts(result){
+        this.wiredResult = result;
+
+        if(result.data){
+            this.contacts = result.data;
+        } else if(result.error){
+            console.error(result.error);
+            this.contacts = [];
+        }
+    }
+
+    handleNotes(event){
+        const id = event.target.dataset.id;
+        this.notesMap[id] = event.target.value;
+    }
+
+    handleComplete(event){
+        const id = event.target.dataset.id;
+
+        markCompleted({ 
+            contactId: id, 
+            notes: this.notesMap[id] || '' 
+        })
+        .then(() => refreshApex(this.wiredResult)) 
+        .catch(error => console.error(error));
+    }
+}
